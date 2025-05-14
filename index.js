06f1cfa319dc7b05ef2cb5809bf0a7376556ed19
Sombr <95926658+lelordd@users.noreply.github.com>
2025-05-14 13:08:31 +0200

    update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = {
     nodeClasses: {
-      Aitable: require('./dist/nodes/Aitable/Aitable.node').Aitable,
+      aitable: require('./dist/nodes/Aitable/Aitable.node').Aitable,
     },
     credentialClasses: {
-      AitableApi: require('./dist/credentials/AitableApi.credentials').AitableApi,
+      aitableApi: require('./dist/credentials/AitableApi.credentials').AitableApi,
     },
   };
